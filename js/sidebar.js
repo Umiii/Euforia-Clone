c353127cee5c72bf43833ca72b192abb12c53ecb
Umeadi Ikem <umy.ikem@gmail.com>
2017-09-30 23:14:07 +0100

    initial interaction for mobile menu

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,20 +4,55 @@
 const navButton = document.querySelector(".nav-control");
 const sliderNav = document.querySelector(".slider-nav");
 const angleLeft = document.querySelector(".angle-left-link");
+const navList = document.querySelector(".nav-list");
+
+
+
 
 navButton.addEventListener('click', function() {
-    if (sliderNav.style.left == "" || sliderNav.style.left === "-555px")
+    var width = document.documentElement.clientWidth;
+    if (width > 1024) {
+        if (sliderNav.style.left == "" || sliderNav.style.left === "-555px") {
+            sliderNav.style.left = "0px";
+            sliderNav.style.zIndex = 999;
+            sliderNav.style.overflowY = "scroll";
+        }
+    }
+    else if (width < 1024)
     {
-        sliderNav.style.left = "0px";
-        sliderNav.style.zIndex = 999;
-        sliderNav.style.overflowY = "scroll";
+        if (navList.style.height === "" || navList.style.height === "0px")
+        {
+            var navListHeight = document.body.clientHeight - 52;
+            navList.style.height = navListHeight + 'px';
+            navList.style.opacity = 1;
+        } else
+        {
+            navList.style.height = 0;
+            navList.style.opacity = 0;
+
+        }
     }
 });
 
 angleLeft.addEventListener('click', function(){
-    if(sliderNav.style.left === "0px")
-    {
-        sliderNav.style.left = "-555px";
+    var width = document.documentElement.clientWidth;
+    if (width > 1024) {
+        if (sliderNav.style.left === "0px") {
+            sliderNav.style.left = "-555px";
 
+        }
+    }
+});
+
+window.addEventListener('resize', function(){
+    var width = document.documentElement.clientWidth;
+    if (width > 1024)
+    {
+        navList.style.height = 'unset';
+        navList.style.opacity = 1;
+    } else
+    {
+        navList.style.height = 0;
+        navList.style.opacity = 0;
     }
 });
